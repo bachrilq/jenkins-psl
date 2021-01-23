bf3849e753bd0f3032536431cbf5d4a6cc087fc0
root <root@docker-workshop-master-0>
2021-01-23 06:58:39 +0000

    add main and pipeline

--- src/com/workshop/main.groovy
+++ src/com/workshop/main.groovy
@@ -6,6 +6,29 @@ import com.workshop.stages.*
 
 def main(script) {
     // Object initialization
+    c = new Config()
+ 
+   // Pipeline specific variable get from injected env
+   // Mandatory variable will be check at details & validation steps
+   def repository_name = ("${script.env.repository_name}" != "null") ? "${script.env.repository_name}" : ""
+   def branch_name = ("${script.env.branch_name}" != "null") ? "${script.env.branch_name}" : ""
+   def git_user = ("${script.env.git_user}" != "null") ? "${script.env.git_user}" : ""
+   def docker_user = ("${script.env.docker_user}" != "null") ? "${script.env.docker_user}" : ""
+   def app_port = ("${script.env.app_port}" != "null") ? "${script.env.app_port}" : ""
+   def pr_num = ("${script.env.pr_num}" != "null") ? "${script.env.pr_num}" : ""
+ 
+   // Pipeline object
+   p = new Pipeline(
+       repository_name,
+       branch_name,
+       git_user,
+       docker_user,
+       app_port,
+       pr_num
+       
+ 
+   )
+
 
     // Pipeline object
 
